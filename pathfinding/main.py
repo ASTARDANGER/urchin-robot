018b3feae3a42866a6e342ea82a1e4a415bc7115
ASTARDANGER <aastardanger@gmail.com>
2024-12-26 21:16:16 +0100

    new trajectory + visual enhancement
    -added square trajectory
    -added gradient color for the path taken by the robot
    -the robot now starts at the first waypoint
    -other minor code enhancement

--- pathfinding/main.py
+++ pathfinding/main.py
@@ -5,27 +5,29 @@ Created on Wed Dec 18 16:44:09 2024
 @author: robin
 """
 import matplotlib.pyplot as plt
+import matplotlib.colors as mplcolors
 import waypoint
 import graph_transition
 import search_funcs
 import math
+import numpy as np
+
 
 
 
 """ parameters """
-starting_position = (0,0) #starting position of the robot ### A MODIFIER POUR LE DEFINIR EN FONCTION DU 1ER WAYPOINT
-tolerance = 0 #tolerance for the approximation of the trajectory
+search_radius = 0 # how far we search for the next waypoint (impact the quality of trajectory's approximation)
 initial_angle = 0 #en degrees
 side = 100 #side size of the triangles
  
 """ initialization """
-## calculation of the searching radius to determine the next waypoint
-search_radius = math.sqrt(3)*side+tolerance #tolerance=0 <=> minimum distance between the robot and the next available waypoints
+## recovery of the waypoints
+waypoints,x,y,center_x,center_y = waypoint.square() ## USER DECIDES OF THE DESIRED PATH HERE
 ## initialization of the robot position
-position_list= [starting_position] #list of all the positions taken by the projected barycenter of the robot
+starting_position = (waypoints[0][0],waypoints[1][0]) ## defined according to the first waypoint
+position_list = [starting_position] #list of all the positions taken by the projected barycenter of the robot
 state_list = [([1,initial_angle],[3,initial_angle+120],[2,initial_angle+240])] #list of all the angles of the current face
-## initialization of the waypoints
-waypoints,x,y,center_x,center_y = waypoint.circle() ## USER DECIDES OF THE DESIRED PATH HERE
+## initialization of the first waypoint
 current_waypoint, index_current_waypoint = search_funcs.search_next_waypoint(starting_position, 0, waypoints, search_radius)
 i = 0
 print(current_waypoint, index_current_waypoint)
@@ -34,7 +36,7 @@ print(current_waypoint, index_current_waypoint)
 ## main loop
 while i < 199: ######TEMPORAIRE
     k=0
-    while not search_funcs.point_dans_triangle(current_waypoint, position_list[-1], side, state_list[-1])  :
+    while not search_funcs.point_in_triangle(current_waypoint, position_list[-1], side, state_list[-1])  :
         angle = search_funcs.angle_waypoint(current_waypoint, position_list[-1])
         new_tuple, new_position = graph_transition.transition(state_list[-1], angle, position_list[-1], math.sqrt(3) * side / 2)
         state_list.append(new_tuple)
@@ -49,6 +51,11 @@ while i < 199: ######TEMPORAIRE
     i = index_last_waypoint
 
 """ display """
+## to plot with a gradient of color
+def colorFader(c1="green",c2="red",mix=0): #fade (linear interpolate) from color c1 (at mix=0) to c2 (mix=1)
+    c1=np.array(mplcolors.to_rgb(c1))
+    c2=np.array(mplcolors.to_rgb(c2))
+    return mplcolors.to_hex((1-mix)*c1 + mix*c2)
 #triangles = [search_funcs.coord_triangle(position_list[i], side, state_list[i]) for i in range(len(state_list))] ## INUTIL?
 ## Desired path plot
 plt.figure(figsize=(8, 8))
@@ -56,21 +63,20 @@ plt.plot(x, y, label="Desired path")
 plt.scatter(waypoints[0],waypoints[1], 5, color = "blue", marker="o", label = "Waypoint")
 ## Planned path plot with starting and ending triangles
 for i in range(len(state_list)) :
-    
     pos_triangle = search_funcs.coord_triangle(position_list[i], side, state_list[i])
     if i ==0 :
         plt.plot([pos_triangle[0][0],pos_triangle[1][0],pos_triangle[2][0],pos_triangle[0][0]],[pos_triangle[0][1],pos_triangle[1][1],pos_triangle[2][1],pos_triangle[0][1]],color ="green")
     elif i == len(state_list)-1 :
         plt.plot([pos_triangle[0][0],pos_triangle[1][0],pos_triangle[2][0],pos_triangle[0][0]],[pos_triangle[0][1],pos_triangle[1][1],pos_triangle[2][1],pos_triangle[0][1]],color ="red")
     else:
-        plt.plot([pos_triangle[0][0],pos_triangle[1][0],pos_triangle[2][0],pos_triangle[0][0]],[pos_triangle[0][1],pos_triangle[1][1],pos_triangle[2][1],pos_triangle[0][1]],color ="lightgray", linestyle='dotted')
+        plt.plot([pos_triangle[0][0],pos_triangle[1][0],pos_triangle[2][0],pos_triangle[0][0]],[pos_triangle[0][1],pos_triangle[1][1],pos_triangle[2][1],pos_triangle[0][1]],color =colorFader("green", "red", i/len(state_list)), alpha=0.4)
 plt.plot([i[0] for i in position_list], [i[1] for i in position_list],color="red", label="Planned path")
 ## Graph settings
 plt.axhline(0, color="black", linewidth=0.5, linestyle="--")  # x axis
 plt.axvline(0, color="black", linewidth=0.5, linestyle="--")  # y axis
 plt.gca().set_aspect('equal', adjustable='box')  # same scale on x and y
-plt.xlim(-2100, 200)  # x limits
-plt.ylim(-1200, 1200)  # y limits
+#plt.xlim(-2100, 200)  # x limits
+#plt.ylim(-1200, 1200)  # y limits
 plt.title("Path planning of the Urchin Robot")
 plt.xlabel("X")
 plt.ylabel("Y")
--- pathfinding/waypoint.py
+++ pathfinding/waypoint.py
@@ -12,36 +12,54 @@ def circle(radius = 1000, N = 1000):
     #Center of the circle
     center_x = -radius
     center_y = 0
-    
     # Waypoints generation
     theta = np.linspace(0, 2 * np.pi, N)
     x = center_x + radius * np.cos(theta)
     y = center_y + radius * np.sin(theta)
-    waypoint =  (x[::5], y[::5])
-    
-    return waypoint,x,y,center_x,center_y 
+    waypoints =  (x[::6], y[::6]) 
+    return waypoints,x,y,center_x,center_y 
 
-def square(side=1000, N=1000): ### A REPRENDRE POUR QUE LE TRACE SOIT DANS LE BON ORDRE
+def square(side=2000, N=1000):
     # Center of the square
-    center_x = -side / 2
+    center_x = 0
     center_y = 0
-    
     # Waypoints generation
-    t = np.linspace(0, 1, N)
-    # Top edge
-    x_top = -side/2 + t * side
-    y_top = side/2 * np.ones_like(x_top)
-    # Right edge
-    x_right = side/2 * np.ones_like(t)
-    y_right = side/2 - t * side
-    # Bottom edge
-    x_bottom = side/2 - t * side
-    y_bottom = -side/2 * np.ones_like(x_bottom)
-    # Left edge
-    x_left = -side/2 * np.ones_like(t)
-    y_left = -side/2 + t * side
-    # Combine the edges
+    N_per_edge = N // 4  # Divide points equally among four edges
+    # Top edge (left to right)
+    x_top = np.linspace(-side/2, side/2, N_per_edge)
+    y_top = np.full_like(x_top, side/2)
+    # Right edge (top to bottom)
+    y_right = np.linspace(side/2, -side/2, N_per_edge)
+    x_right = np.full_like(y_right, side/2)
+    # Bottom edge (right to left)
+    x_bottom = np.linspace(side/2, -side/2, N_per_edge)
+    y_bottom = np.full_like(x_bottom, -side/2)
+    # Left edge (bottom to top)
+    y_left = np.linspace(-side/2, side/2, N_per_edge)
+    x_left = np.full_like(y_left, -side/2)
+    # Combine edges in a continuous path
     x = np.concatenate([x_top, x_right, x_bottom, x_left]) + center_x
     y = np.concatenate([y_top, y_right, y_bottom, y_left]) + center_y
-    waypoint = (x[::5], y[::5])
-    return waypoint, x, y, center_x, center_y
+    waypoints = (x[::5], y[::5])
+    return waypoints, x, y, center_x, center_y
+
+def equilateral_triangle(side=2000, N=1000):
+    # Center of the square
+    center_x = 0
+    center_y = 0
+    # Waypoints generation
+    N_per_edge = N // 3  # Divide points equally among four edges
+    # Top edge (left to right)
+    x1 = np.linspace(-side/2, side/2, N_per_edge)
+    y1 = np.full_like(x_top, 3*sqrt(3)*side)
+    # Right edge (top to bottom)
+    x2 = np.linspace(side/2, -side/2, N_per_edge)
+    y2 = np.full_like(y_right, side/2)
+    # Bottom edge (right to left)
+    x3 = np.linspace(side/2, -side/2, N_per_edge)
+    y3 = np.full_like(x_bottom, -side/2)
+    # Combine edges in a continuous path
+    x = np.concatenate([x1, x2, x3]) + center_x
+    y = np.concatenate([y1, y2, y3]) + center_y
+    waypoints = (x[::5], y[::5])
+    return waypoints, x, y, center_x, center_y
